f82946132ec2343e5401049c5d7e910402f2c9c9
Jason Wu <zwu047@hotmail.com>
2020-03-21 18:27:41 +1300

    add mapper interface

---
+++ src/main/java/ccc/springboot/coconut/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package ccc.springboot.coconut.dao;
+
+import ccc.springboot.coconut.model.entity.User;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    @Select("select * from tb_user")
+    List<User> getAllUsers();
+
+    @Delete("delete from tb_user where id = #{id}")
+    Void delete(Integer Id);
+}
