01c6623b2875cbb8c19fa9399f260bd531cfe88a
Jason Wu <zwu047@hotmail.com>
2020-03-21 16:19:57 +1300

    initial check in

---
+++ src/main/java/ccc/springboot/coconut/CoconutApplication.java
@@ -0,0 +1,13 @@
+package ccc.springboot.coconut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoconutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoconutApplication.class, args);
+    }
+
+}
