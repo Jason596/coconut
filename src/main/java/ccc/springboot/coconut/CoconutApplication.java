5d3970b5cfef336e14013523e89e51dec2a7ca70
Jason Wu <zwu047@hotmail.com>
2020-03-27 20:23:06 +1300

    enable cache

--- src/main/java/ccc/springboot/coconut/CoconutApplication.java
+++ src/main/java/ccc/springboot/coconut/CoconutApplication.java
@@ -2,8 +2,10 @@ package ccc.springboot.coconut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CoconutApplication {
 
     public static void main(String[] args) {
