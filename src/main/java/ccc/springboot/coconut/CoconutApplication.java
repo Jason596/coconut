e587adb930550c11c18e50738af7e2c48f1b942b
Jason Wu <zwu047@hotmail.com>
2020-03-27 21:08:27 +1300

    add active mq class bean

--- src/main/java/ccc/springboot/coconut/CoconutApplication.java
+++ src/main/java/ccc/springboot/coconut/CoconutApplication.java
@@ -1,15 +1,25 @@
 package ccc.springboot.coconut;
 
+
+import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+
+import javax.jms.Queue;
 
 @SpringBootApplication
 @EnableCaching
 public class CoconutApplication {
 
+    @Bean
+    public Queue queue() {
+        return new ActiveMQQueue("active.queue");    // this line of code created an internal activemq. but you could also
+    }                                                      // use an external activemq with specify details in application.yaml.
     public static void main(String[] args) {
         SpringApplication.run(CoconutApplication.class, args);
     }
+
 
 }
