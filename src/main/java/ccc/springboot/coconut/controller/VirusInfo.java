1a145486b04b3f30703e4156c748929eee69cc61
Jason Wu <zwu047@hotmail.com>
2020-04-10 20:37:10 +1200

    create total cases number

--- src/main/java/ccc/springboot/coconut/controller/VirusInfo.java
+++ src/main/java/ccc/springboot/coconut/controller/VirusInfo.java
@@ -1,14 +1,31 @@
 package ccc.springboot.coconut.controller;
 
+import ccc.springboot.coconut.model.entity.VirusStats;
+import ccc.springboot.coconut.service.CovidVarius;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class VirusInfo {
-    @RequestMapping("/covid19")
-    public String virusStats(Model model) {
-    model.addAttribute("testName", "test");
-        return "home";
-    }
+
+  private CovidVarius covidVarius;
+
+  @Autowired
+  public VirusInfo(CovidVarius covidVarius) {
+    this.covidVarius = covidVarius;
+  }
+
+  @RequestMapping("/covid19")
+  public String virusStats(Model model) {
+    List<VirusStats> statsArray = covidVarius.getStatsArray();
+    int totalCases = statsArray.stream().mapToInt(num -> num.getLastConfirmedNum()).sum();
+
+    model.addAttribute("localVirusStats", statsArray);
+    model.addAttribute("totalCases", totalCases);
+    return "home";
+  }
 }
